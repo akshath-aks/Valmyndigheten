5ead3f5c60b3ea69fb166c1b03ebd0cf2d97acb1
Yaning2022 <wyn198877@gmail.com>
2022-09-26 11:54:38 +0200

    changed the test suite

--- tests/testthat/test-Valmyndigheten.R
+++ tests/testthat/test-Valmyndigheten.R
@@ -1,6 +1,11 @@
 result_path<-'18.14c1f613181ed0043d567ae/1663009000443/valresultat-riksdagen-preliminar-jamforande-statistik.xlsx'
 result<-Valmyndigheten_api(result_path)
 
-test_that("testing Valmyndigheten_api function output is correct.", {
+test_that("testing Valmyndigheten_api function can return a given value for a specific set of inputs.", {
   expect_equal(result[[1]]$Parti...1[2], "Centerpartiet")
+})
+
+test_that("with the wrong input Valmyndigheten_api does not work.",{
+  result_path<-'8'
+  expect_error(Valmyndigheten_api(result_path))
 })
